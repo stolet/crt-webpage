36df9583204cd124402328c311486153b6abc85f
Matheus <matheusstolet@hotmail.com>
2026-05-02 01:03:20 +0200

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
-cp -r webpage/* stolet.github.io/
-cd stolet.github.io/
+#!/usr/bin/env bash
+set -euo pipefail
 
-git add .
+git submodule update --init --recursive
+git -C stolet.github.io switch master
+git -C stolet.github.io pull --ff-only origin master
+
+cp -r webpage/* stolet.github.io/
+git -C stolet.github.io add .
 
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
-git commit -m "$msg"
 
-git push origin master
+if git -C stolet.github.io diff --cached --quiet; then
+	echo "No deploy changes to commit."
+else
+	git -C stolet.github.io commit -m "$msg"
+fi
+
+git -C stolet.github.io push origin master
